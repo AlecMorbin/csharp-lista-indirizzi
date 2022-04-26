35fb8a8e1c5e931b81022f6ba9c5beeed90c6ccd
Alec <alemor98@hotmail.it>
2022-04-26 14:43:48 +0200

    Add project files.

---
+++ ListaIndirizzi/Persona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    internal class Persona
+    {
+        private string nome;
+        private string cognome;
+        private string via;
+        private string city;
+        private string province;
+        private int zip;
+
+        public Persona(string nome, string cognome, string via, string city, string province, int zip)
+        {
+            this.nome = nome ?? throw new ArgumentNullException(nameof(nome));
+            this.cognome = cognome ?? throw new ArgumentNullException(nameof(cognome));
+            this.via = via ?? throw new ArgumentNullException(nameof(via));
+            this.city = city ?? throw new ArgumentNullException(nameof(city));
+            this.province = province ?? throw new ArgumentNullException(nameof(province));
+            this.zip = zip;
+        }
+
+        public override string? ToString()
+        {
+            return "-------Persona-------" + "\n" +
+                    "Nome: " + nome + "\n" +
+                    "Cognome: " + cognome + "\n" +
+                    "Via: " + via + "\n" +
+                    "Città: " + city + "\n" +
+                    "Province: " + province + "\n" +
+                    "ZIP: " + zip + "\n" ;
+        }
+    }
+}
---
+++ ListaIndirizzi/Program.cs
@@ -0,0 +1,48 @@
+﻿using ListaIndirizzi;
+string path = "C:\\Users\\Alec\\source\\repos\\ListaIndirizzi\\ListaIndirizzi\\bin\\Debug\\net6.0\\addresses.csv";
+
+List<Persona> persone = new List<Persona>();
+
+bool debug = false;
+
+Console.WriteLine("Premi Y per il debug");
+if(Console.ReadLine() == "y")
+    debug = true;
+List<string> lista = new List<string>();
+
+
+try
+{
+    StreamReader file = File.OpenText(path);
+    file.ReadLine();
+    while (!file.EndOfStream)
+    {
+        string line = file.ReadLine();
+        lista.Add(line);
+        string[] parts = line.Split(',');
+        if (parts.Length == 6)
+        {
+            Persona persona = new Persona(parts[0], parts[1], parts[2], parts[3], parts[4], int.Parse(parts[5]));
+            persone.Add(persona);
+        }
+    }
+    file.Close();
+
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+    Console.WriteLine(error.StackTrace);
+    Console.WriteLine("Errore nella lettura del file");
+}
+foreach (Persona persona in persone)
+{
+    Console.WriteLine(persona.ToString());
+}
+if (debug)
+{
+    foreach (var item in lista)
+    {
+        Console.WriteLine(item);
+    }
+}
